083984fed0dd01bb70647bb1cf767dd208be30e0
Sebastian Law <44045823+SebbyLaw@users.noreply.github.com>
2021-03-22 15:28:51 -0700

    use css instead
    #1 was right

--- index.js
+++ index.js
@@ -1,44 +1,7 @@
 const { Plugin } = require('powercord/entities');
-const { getModule } = require('powercord/webpack');
-const { inject, uninject } = require('powercord/injector');
-const { findInReactTree } = require('powercord/util');
-
 
 module.exports = class RemoveInviteFromUserContextMenu extends Plugin {
     async startPlugin () {
-
-        const dmUserContextMenu = await getModule(m => m.default?.displayName === "DMUserContextMenu");
-        const groupDmUserContextMenu = await getModule(m => m.default?.displayName === "GroupDMUserContextMenu");
-        const guildUserContextMenu = await getModule(m => m.default?.displayName === "GuildChannelUserContextMenu");
-
-        inject(
-            "remove-invite-from-user-context-menu-dmContextPatch",
-            dmUserContextMenu,
-            "default",
-            this.contextPatch
-        );
-        inject(
-            "remove-invite-from-user-context-menu-groupDmContextPatch",
-            groupDmUserContextMenu,
-            "default",
-            this.contextPatch
-        );
-        inject(
-            "remove-invite-from-user-context-menu-guildUserContextPatch",
-            guildUserContextMenu,
-            "default",
-            this.contextPatch
-        );
-    }
-
-    pluginWillUnload() {
-        uninject("remove-invite-from-user-context-menu-dmContextPatch");
-        uninject("remove-invite-from-user-context-menu-groupDmContextPatch");
-        uninject("remove-invite-from-user-context-menu-guildUserContextPatch");
-    }
-
-    contextPatch(args, res) {
-        res.props.children.props.children[4].props.children[3] = null;
-        return res;
+        this.loadStylesheet('style.css');
     }
 }
